f550185de3063af91001dd44d6fc0dc9c7fe8d3a
Lalitowey <91431946+Lalitowey@users.noreply.github.com>
2025-06-08 22:39:26 -0700

    Added AppNavbar component to replace header in default layout

--- frontend/dota-2-project/layouts/default.vue
+++ frontend/dota-2-project/layouts/default.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import sidebar from '@/components/sidebar.vue';
+import AppNavbar from '@/components/AppNavbar.vue';
 </script>
 
 <template>
   <div class="flex h-screen bg-background">
     <sidebar/>
     <div class="flex-1 flex flex-col overflow-hidden">
-      <header class="h-16 border-b flex items-center px-6">
-        <h1 class="text-xl font-semibold">Page Title (from Navbar later)</h1>
-      </header>
+
+      <AppNavbar/>
       <main class="flex-1 p-4 md:p-6 lg:p-8 overflow-y-auto">
         <slot/>
       </main>
