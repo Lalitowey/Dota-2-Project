fc156f43c76516fbdcb54a962274652b060152bf
Lalitowey <91431946+Lalitowey@users.noreply.github.com>
2025-08-03 00:18:50 -0700

    Add command palette toggle functionality using keyboard shortcuts

--- frontend/dota-2-project/layouts/default.vue
+++ frontend/dota-2-project/layouts/default.vue
@@ -1,6 +1,20 @@
 <script setup lang="ts">
-import sidebar from '@/components/sidebar.vue';
+import { ref, watch } from 'vue';
+import { useMagicKeys } from '@vueuse/core';
+import AppSidebar from '@/components/sidebar.vue';
 import AppNavbar from '@/components/AppNavbar.vue';
+import CommandPalette from '@/components/CommandPalette.vue'
+
+const open = ref(false);
+const keys = useMagicKeys();
+const CtrlK = keys['Ctrl + K'];
+const CmdK = keys['Cmd + K'];
+
+watch([CtrlK, CmdK], (v) => {
+  if(v) {
+    open.value = !open.value;
+  }
+});
 </script>
 
 <template>
