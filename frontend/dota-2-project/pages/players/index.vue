002cce3f1392845ee8289fcf39132fb35a57ea43
Lalitowey <91431946+Lalitowey@users.noreply.github.com>
2025-10-20 20:55:17 -0700

    Minor text tweak for consistency

--- frontend/dota-2-project/pages/players/index.vue
+++ frontend/dota-2-project/pages/players/index.vue
@@ -6,7 +6,7 @@ useHead({
 </script>
 <template>
   <div class="container mx-auto py-8">
-    <h1 class="text-2xl font-bold mb-4">Player Search</h1>
+    <h1 class="text-3xl font-bold mb-4">Player Search</h1>
     <p>Use the command palette (CMD + K) to find specific players by their Account ID or Account Name.</p>
   </div>
 </template>
